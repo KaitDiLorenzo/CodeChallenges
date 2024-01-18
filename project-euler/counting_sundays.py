2d9a284d0b85e494611ae405152760c6c4be4aa7
Kaitlyn DiLorenzo <46169510+KaitDiLorenzo@users.noreply.github.com>
2024-01-18 14:35:46 -0600

    Counting Sundays (incomplete)

---
+++ project-euler/counting_sundays.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+#INCOMPLETE....obviously...
+def counting_sundays(start_date, end_date):
+    return start_date
+
+print(counting_sundays(date(1901, 1, 1), date(2000, 12, 31)))
