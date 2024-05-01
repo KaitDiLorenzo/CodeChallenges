3d93e49c9c4da3b71ce18e839da6da02dba01554
Kaitlyn DiLorenzo <46169510+KaitDiLorenzo@users.noreply.github.com>
2024-05-01 14:29:34 -0500

    Updated to solve mathematically vs programmatically

--- project-euler/number_spiral_diagonals.py
+++ project-euler/number_spiral_diagonals.py
@@ -1,20 +1,12 @@
+import math
+
 def number_spiral_diagonals(n):
     if n < 1 or n % 2 == 0:
         raise ValueError('The value of n is invalid.')
     
-    addend = 2
-    counter = 0
-    sum = 1
-    value = 1
-
-    while value < n**2:
-        value += addend
-        sum += value
-        counter += 1
-
-        if counter == 4:
-            counter = 0
-            addend += 2
+    sum = 0
+    for i in range(1, 2 * n):
+        sum += (2 * math.ceil(i / 4) - 1) ** 2 + 2 * math.ceil(i / 4) * ((i - 1) % 4)
 
     return sum
 
